01cb9acfa90a736e5d30047d1a9e8fc5f7d81b3d
system <system@iiidevops.org>
2023-04-10 06:41:22 +0000

    "範本 commit"

---
+++ app/src/App.js
@@ -0,0 +1,49 @@
+import { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    
+    this.state={
+      monsters:[],
+    }
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState(() => {
+        return {monsters:users}
+      },()=>{
+        console.log(this.state)
+      }));
+  }
+  render(){
+    console.log(process.env.REACT_APP_APPURL)
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="xxxxx"
+          onChange={(event) => {
+            console.log(event.target.value);
+            const filterMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.includes(event.target.value);
+            });
+          }}
+        />
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
